484f7f6821c7db8c3938d8c3abb198035a104054
Kleber Soares <kleberr@msn.com>
2019-01-13 13:16:55 -0300

    Add configs

---
+++ djangoherokuaws/utils.py
@@ -0,0 +1,8 @@
+"""
+Utils
+"""
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+StaticRootS3Boto3Storage = lambda: S3Boto3Storage(location='static')
+MediaRootS3Boto3Storage = lambda: S3Boto3Storage(location='media')
